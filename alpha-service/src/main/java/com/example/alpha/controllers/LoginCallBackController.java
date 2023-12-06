fa8e8a30ef9aef25082b31c478afd842e82b0d33
Sundaravel Varatharajan <sundaravel10@gmail.com>
2023-12-06 12:21:00 +0530

    Modified Cognito Related code

--- alpha-service/src/main/java/com/example/alpha/controllers/LoginCallBackController.java
+++ alpha-service/src/main/java/com/example/alpha/controllers/LoginCallBackController.java
@@ -1,10 +1,12 @@
 package com.example.alpha.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/aws")
 public class LoginCallBackController {
 
     @GetMapping("/login/oauth2/code/cognito")
