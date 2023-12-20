2ed3716c10d8634c18d8901e80713b370fcedc69
Sundaravel Varatharajan <sundaravel10@gmail.com>
2023-12-20 16:42:29 +0530

    Interceptor included

--- alpha-service/src/main/java/com/example/alpha/config/SecurityConfig.java
+++ alpha-service/src/main/java/com/example/alpha/config/SecurityConfig.java
@@ -3,19 +3,13 @@ package com.example.alpha.config;
 import com.example.alpha.interceptors.RestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig implements WebMvcConfigurer {
 
-    @Bean
+    /*@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
@@ -24,7 +18,7 @@ public class SecurityConfig implements WebMvcConfigurer {
                 )
                 .httpBasic(withDefaults());
         return http.build();
-    }
+    }*/
 
     @Bean
     public RestInterceptor restInterceptor() {
--- alpha-service/src/main/java/com/example/alpha/interceptors/RestInterceptor.java
+++ alpha-service/src/main/java/com/example/alpha/interceptors/RestInterceptor.java
@@ -4,14 +4,18 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import java.util.Objects;
+
 public class RestInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
         final String token = request.getHeader("Authorization");
-        if (token == null || token.isEmpty()) {
+        if (Objects.nonNull(token) && !token.isEmpty()) {
             System.out.println("Token Value " + token);
+        }else{
+            System.out.println("Unauthorized");
         }
         return true;
     }
